2dc01e006ce7453d2d30d073221ac2a3aa4af3ce
dharanideva0 <dharani@estreamly.com>
2024-10-02 18:05:22 +0530

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('pg with NestJS')
+    .setDescription('postgres with NestJS API description')
+    .setVersion('1.0')
+    // .addTag('pgwithnest')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
